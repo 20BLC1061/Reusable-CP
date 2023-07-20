75ef0df8d27550a5f45d57db62bb23751dbc077c
PavanPk1 <pavankumarpulipati16313@gmail.com>
2023-07-20 15:13:10 +0000

    First Commit

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const Btn = () => <button className="btn">Show More</button>
+const ReusableBanners = props => {
+  const {bannerCardsList} = props
+  const {headerText, description, className} = bannerCardsList
+  return (
+    <li>
+      <div className={`card ${className}`}>
+        <div>
+          <h1 className="heading">{headerText}</h1>
+          <p className="para">{description}</p>
+          <Btn />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default ReusableBanners
